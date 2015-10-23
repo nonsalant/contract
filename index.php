428d8e8bef828780da97651f2a252b4e1998a7ac
Stefan Matei <nonsalant@gmail.com>
2015-10-23 15:19:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -254,7 +254,7 @@ function printContract() {
   window.print();
 }
 function generatePdf() {
-  window.location.href = '//pdfcrowd.com/url_to_pdf/?use_print_media=1&footer_text=page%20%p%20%of%20%n&hmargin=1cm&pdf_name=<?php echo $pdfName; ?>';
+  window.location.href = '//pdfcrowd.com/url_to_pdf/?use_print_media=1&footer_text=page%20%p%20of%20%n&hmargin=1cm&pdf_name=<?php echo $pdfName; ?>';
 }
 </script>
 
