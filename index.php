5066706147a0940e64cae30ceb098be1066995ff
Stefan Matei <nonsalant@gmail.com>
2015-10-23 17:43:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -166,7 +166,7 @@ button:hover {
 
 <div id="content">
   
-  <h1>Contract of work for design and redesign</h1>
+  <h1>Contract of work for website design and development</h1>
   <p>Contract contents in HTML format.</p>
   
   <h2>1. Agreement of parties</h2>
