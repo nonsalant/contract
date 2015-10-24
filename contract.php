61381aa984beae58785cf2e9aa369fddb49ba2d2
Stefan Matei <nonsalant@gmail.com>
2015-10-24 18:40:28 +0300

    Update contract.php

--- contract.php
+++ contract.php
@@ -2,13 +2,14 @@
  
 // Single File PHP Contract
 
+$email = ""; // Notify when contract is signed
+
 $phpName  = basename($_SERVER['PHP_SELF']) ? basename($_SERVER['PHP_SELF']) : 'index.php';
 $fileName = substr($phpName , 0, -4); // without extension
 $htmlName = $fileName.'.html';
 $pdfName = $fileName.'.pdf';
 $contractData = stripslashes ( $_POST['contractdata'] ); 
 $ip = get_client_ip_env();
-$email = "";
 
 // If the HTML file exists, redirect to it
 redirectIfExists();
