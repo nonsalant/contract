931a93284b0016f43f0c846289fa9d6018e65762
Stefan Matei <nonsalant@gmail.com>
2015-10-24 18:51:27 +0300

    Update contract.php

--- contract.php
+++ contract.php
@@ -4,7 +4,7 @@
 Single File PHP Contract
 
 Upload this file to your server and send a link to your client. 
-(You can rename this php, for example if there are other contracts in that folder.)
+(You can rename this php, for example if there are other contracts in the same folder.)
 
 Once they have signed, this php file will delete itself and leave behind an HTML version 
 of the signed contract that can be printed or downloaded as a PDF.
