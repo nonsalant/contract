3d8b10ed914ff955ffbe56e1d042a7f7ef72a377
Stefan Matei <nonsalant@gmail.com>
2015-10-25 19:35:30 +0200

    Create process.php

---
+++ contract-parts/process.php
@@ -0,0 +1,24 @@
+<?php
+$part4 = stripslashes ( $_POST['signature_capture'] ); 
+if( $part4 ) {
+  $part1 = file_get_contents('contract-parts/header.txt');
+  $part2 = stripslashes ( $_POST['html_capture'] ); 
+  $part3 = file_get_contents('contract-parts/middle.txt');
+  $part5 = file_get_contents('contract-parts/footer.txt');
+  
+  $filename = stripslashes ( $_POST['file_name'] );
+  if (!$filename) { $filename = 'contract.php'; }
+  
+  header('Content-Disposition: attachment; filename='.$filename);    
+  header('Content-Type: text/force-download');
+  header('Content-Type: text/plain');
+  header('Content-Type: application/download');
+  header('Content-Description: File Transfer');
+  
+  echo $part1.$part2.$part3.$part4.$part5;
+}
+else {
+  echo '<h2 style="text-align:center;">No signature received</h2>';
+  die();
+}
+?>
