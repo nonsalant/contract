2e890277fdb3a5e0663ae567b5c944f4504c46bc
Stefan Matei <nonsalant@gmail.com>
2015-10-25 15:44:51 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>Contract</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+<style>
+@import url(http://fonts.googleapis.com/css?family=Libre+Baskerville:400,700,400italic);
+@import url(http://fonts.googleapis.com/css?family=Arapey);
+@import url(http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700);
+@import url(http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800);
+</head>
+<body>
+<ul class="top-menu">
+<li class="currentt">
+  <a title="WP Support & Development" href="http://vileworks.com" 
+     style="letter-spacing:.2em; font-size:1.1em">
+    <span style="color:#bf7883; text-shadow:none;">
+      Vile</span><span style="font-weight:300;">Works
+    </span>
+  </a>
+</li>
+<li id="menu-item-1" ><a title="WordPress Develoment Services" href="/services/">Services</a></li>
+<li id="menu-item-2" ><a title="WordPress Design Portfolio" href="/portfolio/">Portfolio</a></li>
+<li id="menu-item-3" ><a title="Resources & Blog" href="/resources/">Resources</a></li>
+<li id="menu-item-4" ><a title="Contact Us" href="/contact/">Contact</a></li>
+</ul>
+
+<a class="site-title" href="http://www.vileworks.com" title="WP Support & Development">VileWorks</a>
+
+<div id="content">
+  
+  <h1>Signable <br> Contract.php</h1>
+  <hr>
+  <h2 style="text-align:center;">You FTP it to your server and send your client a link to it&#8230;</h2>
+  <h2 style="text-align:center; font-family:'Open Sans Condensed', sans; font-weight:300; line-height:1.5em; letter-spacing:.05em">
+    Once <strong>signed</strong><svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 8 8">
+      <path d="M6 0l-1 1 2 2 1-1-2-2zm-2 2l-4 4v2h2l4-4-2-2z" />
+    </svg> by your client, this php file will delete itself from your server and leave behind an HTML version of the contract 
+     that can be 
+    <strong>printed</strong><svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 8 8">
+      <path d="M2 0v2h4v-2h-4zm-1.91 3c-.06 0-.09.04-.09.09v2.81c0 .05.04.09.09.09h.91v-2h6v2h.91c.05 0 .09-.04.09-.09v-2.81c0-.06-.04-.09-.09-.09h-7.81zm1.91 2v3h4v-3h-4z" />
+    </svg> 
+    or downloaded as a <strong>PDF</strong><svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 8 8">
+      <path d="M0 0v8h7v-4h-4v-4h-3zm4 0v3h3l-3-3zm-3 2h1v1h-1v-1zm0 2h1v1h-1v-1zm0 2h4v1h-4v-1z" />
+    </svg>
+  </h2>
+  <hr>
+  
+  <svg xmlns="http://www.w3.org/2000/svg" class="large-icon" viewBox="0 0 8 8">
+    <path d="M5 0l-3 6h1l3-6h-1zm-4 1l-1 2 1 2h1l-1-2 1-2h-1zm5 0l1 2-1 2h1l1-2-1-2h-1z" transform="translate(0 1)" />
+  </svg>
+  <h2>1. Contract HTML</h2>
+  <p>The contract copy should be in HTML format. 
+    Write it below, or paste its <span style="font-family:'Open Sans Condensed'; letter-spacing:.25px;font-size:1.2em; ">&lt;&gt; source code</span> from somewhere else, and replace 
+    the default text in the editor.
+  [Drag corner to resize editor box.]
+  </p>
+  <textarea id="input-html">
+  <h1>Contract of work for website design and development</h1>
+  <p>Between <b>Alice</b>, referred to below as "Designer",
+  and <b>Bob</b>, referred to below as "Customer".</p>
+  
+  <h2>1. Agreement of parties</h2>
+  <p>Customer hires Designer to redesign the current website, <b>bobswebsite.com</b>, 
+    for the estimated total price of <b>$PRICE</b>.</p>
+  <p>The agreed payment plan is at the end of the document.</p>
+  <p>Designer agrees to provide quality service and to answer to the 
+    Customer’s requests in a timely manner. The services provided will be:</p>      
+  <ol style="list-style-type: lower-alpha">
+    <li>service1;</li>
+    <li>service2;</li>
+    <li>service3;</li>
+    <li>service4.</li>
+  </ol>
+
+<h2>2. Legal matters and copyrights</h2>
+<p>The Customer will guarantee to the Designer that any elements of text, graphics, photos, trademarks or other artwork that the Customer provides for inclusion in the website are either owned by him or that he has the permission to use them. When the Designer receives the final payment, copyright is automatically assigned as follows:
+Customer will own the graphics, virtual elements, text content photographs and other data provided, unless someone else owns them.
+The Designer owns the XHTML markup, CSS and other code and he licenses it to the Customer for use on only this project.
+Designer can reserve the right to display, with Customer’s consent, the work as part of the portfolio.</p>  
+
+<h2>3. Term and termination</h2>
+<ol style="list-style-type: lower-alpha">
+  <li>This contract shall commence upon the first payment, as outlined in the payment plan and shall remain effective until the services are completed and delivered.</li>
+  <li>This contract may be terminated at any time by either party effective immediately upon notice, or the mutual agreement of the parties.</li>
+  <li>In the event of termination, Designer shall be compensated for the service performed through the date of termination in the amount (a) any advanced payment, (b) 
+    a prorated portion of the fees due, or (c) hourly fees for work performed by the Designer at the time of the termination, whichever is greater, together with any 
+    additional costs incurred trough and up to the date of cancellation.</li>
+  <li>This Contract may be modified by parties; any modification must be in writing.</li>
+  <li>All notices to be given hereunder shall be transmitted in writing by electronic mail and will only be effective upon return confirmation.</li>
+</ol>
+
+<h2>4. Force majeure</h2>
+<p>Designer shall not be deemed in breach of this contract if Designer is unable to complete the services or any portion by reason of fire, earthquake, labor dispute, illness, internet breaches or any technical issues that may appear beyond Designer’s control. Upon occurrence of any Force Majeure Event, Designer shall give notice to the Customer of his inability to perform or of delay in completing the services and shall propose revisions to the schedule for completion of the services.</p>  
+
+<h2>5. Payment plan</h2>
+<p>Payment shall be done as follows:</p>
+<ul>
+  <li>50% of total estimated fee will be required to commence work, after this contract has been approved and signed by both parties.</li>
+  <li>50% upon project closure.</li>
+</ul>
+<p>Any extra time required outside the project timeline/services mentioned at point 1 of this contract, will be billed at a rate
+of <b>$FEE</b> per hour.</p>
+<!--
+You can generate a new signature at www.cdpn.io/JYpjvE
+--></textarea>
+  
+  <svg xmlns="http://www.w3.org/2000/svg" class="large-icon right" viewBox="0 0 8 8">
+    <path d="M6 0l-1 1 2 2 1-1-2-2zm-2 2l-4 4v2h2l4-4-2-2z" />
+  </svg>
+  <h2>2. Your signature</h2>
+  <p>This goes on the bottom of the contract. Use your mouse cursor or the tip of your finger to sign in the dotted box below.
+  <br>[<a href="#" id="reset">Reset sigature box?</a>]</p>
+  
+  <div id="signature"></div>
+  <!--<div class="buttons">
+    <button id="reset" type="button">Reset signature</button>
+    <button id="preview" type="button" style="display:none;">Preview contract</button>
+  </div>-->
+
+  <svg xmlns="http://www.w3.org/2000/svg" class="large-icon" viewBox="0 0 8 8">
+    <path d="M4.5 0c-1.21 0-2.27.86-2.5 2-1.1 0-2 .9-2 2s.9 2 2 2h4.5c.83 0 1.5-.67 1.5-1.5 0-.65-.42-1.29-1-1.5v-.5c0-1.38-1.12-2.5-2.5-2.5z" transform="translate(0 1)" />
+  </svg>
+  <h2>3. Download & deploy</h2>
+  <form method="post" id="signature-form">
+    <p>Get the contract and upload it to your server using FTP.</p>
+    <button id="submit">
+      <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="12px" height="12px" viewBox="0 0 8 8">
+        <path d="M3 0v3h-2l3 3 3-3h-2v-3h-2zm-3 7v1h8v-1h-8z" />
+      </svg> Download Contract
+    </button> <small>contract-4015ce.php (you can rename this)</small>
+    
+    <input type="hidden" id="signature_capture" name="contractdata">
+    <input type="hidden" id="html_capture" name="contractdata">
+    
+  </form>
+  
+  <svg xmlns="http://www.w3.org/2000/svg" class="large-icon" viewBox="0 0 8 8">
+    <path d="M0 0v1l4 2 4-2v-1h-8zm0 2v4h8v-4l-4 2-4-2z" transform="translate(0 1)" />
+  </svg>
+  <h2>4. Ask your client to review and sign.</h2>
+  <p>Send your client a link to the contract (hosted under your domain).
+  </p>
+  <!--<p>The link could be short and random or long and descriptive, but not very obvious to guess: you probably don't want some clients to be able to find other clients' contracts.</p>-->
+  
+  <hr>
+  <h3 style="text-align:center;">Resources</h3>
+  <ul class="resources two-columns">
+    <li><a target="_blank" href="http://vileworks.com/contract/demo.php">
+      view a signable <strong>demo <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 8 8">
+  <path d="M4.03 0c-2.53 0-4.03 3-4.03 3s1.5 3 4.03 3c2.47 0 3.97-3 3.97-3s-1.5-3-3.97-3zm-.03 1c1.11 0 2 .9 2 2 0 1.11-.89 2-2 2-1.1 0-2-.89-2-2 0-1.1.9-2 2-2zm0 1c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1c0-.1-.04-.19-.06-.28-.08.16-.24.28-.44.28-.28 0-.5-.22-.5-.5 0-.2.12-.36.28-.44-.09-.03-.18-.06-.28-.06z"
+  transform="translate(0 1)" /></svg></strong></a></li>
+    <li><a target="_blank" href="https://github.com/nonsalant/contract/blob/master/contract.php">
+        get contract.php from <strong>github <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 8 8">
+  <path d="M.09 0c-.06 0-.09.04-.09.09v7.81c0 .05.04.09.09.09h7.81c.05 0 .09-.04.09-.09v-7.81c0-.06-.04-.09-.09-.09h-7.81zm1.41.78l1.72 1.72-1.72 1.72-.72-.72 1-1-1-1 .72-.72zm2.5 2.22h3v1h-3v-1z" />
+</svg></strong></a></li>
+    <li><a target="_blank" href="https://github.com/nonsalant/contract/issues">
+      report an <strong>issue <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 8 8">
+  <path d="M3.5 0c-1.19 0-1.98 1.69-1.19 2.5-.09.07-.2.14-.28.22l-1.31-.66a.5.5 0 0 0-.34-.06.5.5 0 0 0-.09.94l1.16.56c-.09.16-.19.33-.25.5h-.69a.5.5 0 0 0-.09 0 .5.5 0 1 0 .09 1h.5c0 .23.02.45.06.66l-.78.41a.5.5 0 1 0 .44.88l.66-.34c.25.46.62.85 1.03 1.09.35-.19.59-.44.59-.72v-1.44a.5.5 0 0 0 0-.09v-.81a.5.5 0 0 0 0-.22c.05-.23.26-.41.5-.41.28 0 .5.22.5.5v.88a.5.5 0 0 0 0 .09v.06a.5.5 0 0 0 0 .09v1.34c0 .27.24.53.59.72.41-.25.79-.63 1.03-1.09l.66.34a.5.5 0 1 0 .44-.88l-.78-.41c.04-.21.06-.43.06-.66h.5a.5.5 0 1 0 0-1h-.69c-.06-.17-.16-.34-.25-.5l1.16-.56a.5.5 0 0 0-.31-.94.5.5 0 0 0-.13.06l-1.31.66c-.09-.08-.19-.15-.28-.22.78-.83 0-2.5-1.19-2.5z"
+  /></svg></strong></a></li>
+    
+    <li><a target="_blank" href="http://www.wpbeginner.com/showcase/6-best-ftp-clients-for-wordpress-users/">
+      recommended <strong>ftp clients <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 8 8">
+  <path d="M4 0c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 1c.33 0 .64.09.94.19-.21.2-.45.38-.41.56.04.18.69.13.69.5 0 .27-.42.35-.13.66.35.35-.64.98-.66 1.44-.03.83.84.97 1.53.97.42 0 .53.2.5.44-.54.77-1.46 1.25-2.47 1.25-.38 0-.73-.09-1.06-.22.22-.44-.28-1.31-.75-1.59-.23-.23-.72-.14-1-.25-.09-.27-.18-.54-.19-.84.03-.05.08-.09.16-.09.19 0 .45.38.59.34.18-.04-.74-1.31-.31-1.56.2-.12.6.39.47-.16-.12-.51.36-.28.66-.41.26-.11.45-.41.13-.59-.06-.03-.13-.1-.22-.19.45-.27.97-.44 1.53-.44zm2.31 1.09c.18.22.32.46.44.72 0 .01 0 .02 0 .03-.04.07-.11.11-.22.22-.28.28-.32-.21-.44-.31-.13-.12-.6.02-.66-.13-.07-.18.5-.42.88-.53z"
+  /></svg></strong></a></li>
+    <li><a target="_blank" href="http://www.smashingmagazine.com/2012/08/free-download-useful-legal-documents-for-designers-pdf/">
+      useful <strong>legal documents <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 8 8">
+  <path d="M4 0c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 1c.33 0 .64.09.94.19-.21.2-.45.38-.41.56.04.18.69.13.69.5 0 .27-.42.35-.13.66.35.35-.64.98-.66 1.44-.03.83.84.97 1.53.97.42 0 .53.2.5.44-.54.77-1.46 1.25-2.47 1.25-.38 0-.73-.09-1.06-.22.22-.44-.28-1.31-.75-1.59-.23-.23-.72-.14-1-.25-.09-.27-.18-.54-.19-.84.03-.05.08-.09.16-.09.19 0 .45.38.59.34.18-.04-.74-1.31-.31-1.56.2-.12.6.39.47-.16-.12-.51.36-.28.66-.41.26-.11.45-.41.13-.59-.06-.03-.13-.1-.22-.19.45-.27.97-.44 1.53-.44zm2.31 1.09c.18.22.32.46.44.72 0 .01 0 .02 0 .03-.04.07-.11.11-.22.22-.28.28-.32-.21-.44-.31-.13-.12-.6.02-.66-.13-.07-.18.5-.42.88-.53z"
+  /></svg></strong></a></li>
+    <li><a target="_blank" href="http://www.wikihow.com/Write-a-Freelance-Contract">
+      wikihow: <strong>freelance contract <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 8 8">
+  <path d="M4 0c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 1c.33 0 .64.09.94.19-.21.2-.45.38-.41.56.04.18.69.13.69.5 0 .27-.42.35-.13.66.35.35-.64.98-.66 1.44-.03.83.84.97 1.53.97.42 0 .53.2.5.44-.54.77-1.46 1.25-2.47 1.25-.38 0-.73-.09-1.06-.22.22-.44-.28-1.31-.75-1.59-.23-.23-.72-.14-1-.25-.09-.27-.18-.54-.19-.84.03-.05.08-.09.16-.09.19 0 .45.38.59.34.18-.04-.74-1.31-.31-1.56.2-.12.6.39.47-.16-.12-.51.36-.28.66-.41.26-.11.45-.41.13-.59-.06-.03-.13-.1-.22-.19.45-.27.97-.44 1.53-.44zm2.31 1.09c.18.22.32.46.44.72 0 .01 0 .02 0 .03-.04.07-.11.11-.22.22-.28.28-.32-.21-.44-.31-.13-.12-.6.02-.66-.13-.07-.18.5-.42.88-.53z"
+  />
+</svg></strong></a></li>
+    
+  </ul>  
+  
+  <hr>
+  <p class="footer">Created by <a href="http://twitter.com/nonsalant/">Stefan Matei</a> for <a href="http://vileworks.com">VileWorks.com</a></p>
+</div> <!--#content-->
+
+<script src="http://www.vileworks.com/combined.js"></script>
+<script>
+tinymce.init({
+    selector: "#input-html",
+    content_css : "http://vileworks.com/contract/editor-style.css", 
+    min_height: 300,
+    plugins: [
+        "advlist autolink lists link image charmap print preview anchor",
+        "searchreplace visualblocks code fullscreen",
+        "insertdatetime media table contextmenu paste"
+    ],
+    toolbar: "code undo redo | styleselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent"
+});
+
+$('#signature').jSignature();
+var $sigdiv = $('#signature');
+var datapair = $sigdiv.jSignature('getData', 'svgbase64');
+
+$('#signature').bind('change', function(e) {
+  var data = $('#signature').jSignature('getData');
+  $("#signature_capture").val(data);
+  //$("#signature_capture").show();
+});
+  
+$('#reset').click(function(e){
+  $('#signature').jSignature('clear');
+  var data = $('#signature').jSignature('getData');
+  $('#signature_capture').val('');
+  e.preventDefault();
+});
+
+$('#html_capture').val($('#input-html').val());
+$('#input-html').bind('blur', function(e) {
+  $('#html_capture').val($(this).val());
+});
+  
+
+$('body').niceScroll({
+    cursoropacitymin: 0.5,
+    cursoropacitymax: 1,
+    cursorwidth: "15px",
+    cursorborder: "1px solid rgba(255,255,255,0.5)", // css definition for cursor border
+    cursorborderradius: "3px", // border radius in pixel for cursor
+    scrollspeed: 60, // scrolling speed
+    mousescrollstep: 60, // scrolling speed with mouse wheel (pixel)
+    bouncescroll: true
+});
+
+$(document).scrollsnap({
+  snaps: '#mceu_13, #input-html_ifr',
+  proximity: 85
+});
+
+// Lazy Load
+$('img, svg').lazyload();
+</script>
+
+</body>
+</html>
