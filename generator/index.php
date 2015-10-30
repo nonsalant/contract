a7954fde1505c6f9134a057b78206db042cda766
Stefan Matei <nonsalant@gmail.com>
2015-10-30 21:25:49 +0200

    Update index.php

--- generator/index.php
+++ generator/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
-<title>Contract</title>
+<title>Contract Generator</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
 <style>
 @import url(css/style.css);
