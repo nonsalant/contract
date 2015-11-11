c23c5cdd8124844bcb58861fea53d18b7a1b256c
Stefan Matei <nonsalant@gmail.com>
2015-11-11 13:45:23 -0800

    Update index.js

--- generator/js/index.js
+++ generator/js/index.js
@@ -1,7 +1,7 @@
 // WYSIWYG editor for contract copy
 tinymce.init({
-  selector: "#input-html",
-  content_css : "http://vileworks.com/contract/generator/css/editor-style.css", 
+  selector: "#contract_html",
+  content_css : "http://vileworks.com/contract/generator/css/editor-style.css",
   min_height: 300,
   plugins: [
     "advlist autolink lists link image charmap print preview anchor",
@@ -11,11 +11,19 @@ tinymce.init({
   toolbar: "code undo redo | styleselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent"
 });
 
-// Put signature and contract html in the same form as download button
+// Set UNIX timestamp as file_name
+unix = Math.round(+new Date()/1000);
+// unix += '.php';
+//$('#signature-form small em').html(unix);
+$('#file_name').val(unix);
+
+// Put the various data in the same form as download button
 $('#signature-form').submit(function(){
   tinyMCE.triggerSave(); // Put tinyMCE contents back in textarea!
-  $('#html_capture').val( $('#input-html').val() );
-  $("#signature_capture").val( $('#signature').jSignature('getData') );
+  $('#contract_html_capture').val( $('#contract_html').val() );
+  $('#signature_capture').val( $('#signature').jSignature('getData') );
+  $('#client_email_capture').val( $('#client_email').val() );
+  $('#dev_email_capture').val( $('#dev_email').val() );
 });
 
 // jSignature
@@ -32,7 +40,7 @@ $('#signature').bind('change', function(e) {
 });
 // Redundant if tinyMCE is used
 // $('#html_capture').val($('#input-html').val());
-// $('#input-html').bind('input propertychange', function(e) { 
+// $('#input-html').bind('input propertychange', function(e) {
 //   $('#html_capture').val($(this).val()).attr('type','text');
 // });
 
@@ -45,20 +53,21 @@ $('#reset').click(function(e){
 });
 
 // Set a random name for the file to be downloaded
-randomFileName = 'contract-'+makeId() +'.php'; 
-$('#signature-form small em').html(randomFileName); 
-$('#file_name').val(randomFileName); 
-function makeId() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for( var i=0; i < 5; i++ )
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  return text;
-}
+// randomFileName = 'contract-'+makeId() +'.php';
+// $('#signature-form small em').html(randomFileName);
+// $('#file_name').val(randomFileName);
+// function makeId() {
+//   var text = "";
+//   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//   for( var i=0; i < 5; i++ )
+//     text += possible.charAt(Math.floor(Math.random() * possible.length));
+//   return text;
+// }
+
 
 // Print preview of external demo page
 $('#print-preview').click(function(e){
-  printPage('../demo.php');
+  printPage('../demo.html');
   e.preventDefault();
 });
 //From https://developer.mozilla.org/en-US/docs/Printing#Print_an_external_page_without_opening_it
