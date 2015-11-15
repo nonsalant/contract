9e374fb5c20acf2fbbd82ac5902fec3a5c53f787
Stefan Matei <nonsalant@gmail.com>
2015-11-15 22:10:45 +0200

    Fixed some broken links
    Since demo was renamed to test

--- generator/index.php
+++ generator/index.php
@@ -36,17 +36,19 @@
   <h2 style="text-align:center;">You FTP it to your server and send your client  a link to it&#8230;</h2>
   <h2 style="text-align:center; font-family:'Open Sans Condensed', sans; font-weight:300; line-height:1.5em; letter-spacing:.05em">
     Once
-    <strong>signed</strong><svg class="icon" viewBox="0 0 8 8">
+    <a title="Test signature in new window" href="../test.php" target="_blank">
+      <strong>signed</strong><svg class="icon" viewBox="0 0 8 8">
       <use xlink:href="css/sprite.svg#pencil"></use>
-    </svg>
+      </svg>
+    </a>
     by your client, this php file will delete itself from your server and leave behind an HTML version of the contract that can be
-    <a id="print-preview" title="Print demo" href="../demo.html" target="_blank">
+    <a id="print-preview" title="Print demo" href="../test.html" target="_blank">
       <strong>printed</strong><svg class="icon" viewBox="0 0 8 8">
         <use xlink:href="css/sprite.svg#print"></use>
       </svg>
     </a>
     or downloaded as a
-    <a title="PDF demo" href="../demo.pdf" target="_blank">
+    <a title="PDF demo" href="../test.pdf" target="_blank">
       <strong>PDF</strong><svg class="icon" viewBox="0 0 1024 1024">
         <use xlink:href="css/sprite.svg#pdf"></use>
       </svg>
