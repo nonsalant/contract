450e9923fb1cf379859fda38d8b2dadaaefe59e9
Stefan Matei <nonsalant@gmail.com>
2023-12-30 00:40:22 +0200

    js/localStorage + add button size and other  style integrations

--- generator/data/contract-data.js
+++ generator/data/contract-data.js
@@ -6,5 +6,11 @@ export const contractData = {
     dev: {
         name: "",
         email: ""
-    }
+    },
+    // shortcodes: []
+    shortcodes: [
+        { name: '[price]', value: '$1000' },
+        { name: '[date]', value: '31/12/2024' },
+        { name: '[hourly-rate]', value: '$50' },
+    ]
 }
